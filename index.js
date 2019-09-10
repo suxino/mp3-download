dd12696d4156a55ddc7528201b3a4916019872fb
Matus Tuhy <matustuhy@gmail.com>
2019-09-10 19:11:48 +0200

    Initial working commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const YoutubeMp3Downloader = require("./lib/YoutubeMp3Downloader");
+const url = require('url');
+
+const YD = new YoutubeMp3Downloader({
+    "ffmpegPath": "./ffmpeg/ffmpeg",
+    "outputPath": "./output",
+    "youtubeVideoQuality": "highest",
+    "queueParallelism": 40,
+    "progressTimeout": 2000
+});
+
+YD.on("progress", function(progress) {
+    console.log(JSON.stringify(progress));
+});
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('url.txt')
+});
+
+lineReader.on('line', (line) => {
+    const videoId = url.parse(line, true).query.v;
+    YD.download(videoId)
+});
