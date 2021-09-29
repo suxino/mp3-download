705066ec5e42f9750e43c2818d2fb9784900ba54
Matus Tuhy <matustuhy@gmail.com>
2021-09-29 16:06:55 +0200

    Update logging

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const YoutubeMp3Downloader = require("youtube-mp3-downloader");
 
 const YD = new YoutubeMp3Downloader({
-    "ffmpegPath": "/usr/local/Cellar/ffmpeg/4.3.1_9/bin/ffmpeg",
+    "ffmpegPath": "/usr/local/Cellar/ffmpeg/4.4_2/bin/ffmpeg",
     "outputPath": "./output",
     "youtubeVideoQuality": "highest",
     "queueParallelism": 40,
     "progressTimeout": 2000
 });
 
-YD.on("progress", function(progress) {
+YD.on("progress", (progress) => {
     console.log(JSON.stringify(progress));
 });
+
+YD.on("error", (error) => {
+    console.error(error)
+})
+
+YD.on("queueSize", (total) => {
+    console.log(`Queue size changed. New total - ${total}`)
+})
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('url.txt')
 });
 
 lineReader.on('line', (line) => {
-    try {
-        YD.download(new URL(line).searchParams.get('v'))
-    } catch (e) {
-        console.log(e)
-    }
+    YD.download(new URL(line).searchParams.get('v'))
 });
